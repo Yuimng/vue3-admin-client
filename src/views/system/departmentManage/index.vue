63197387194222851730ba5fdba823a420d3b594
yuimng <944307891@qq.com>
2024-06-20 00:04:34 +0800

    页面初始化

--- src/views/system/departmentManage/index.vue
+++ src/views/system/departmentManage/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="">部门管理</div>
+  <div class="card department-manage">
+    <h3 class="title">部门管理（待完善）</h3>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.department-manage {
+  height: 100%;
+  .title {
+    text-align: center;
+  }
+}
+</style>
